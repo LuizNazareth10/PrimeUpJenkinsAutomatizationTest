7bcbcea3324f86bd00ab462f600b1e9e8fc30d30
Luiz Nazareth <luizgustavonazareth4@gmail.com>
2025-07-09 17:24:52 -0300

    Melhorando o Dockerfile do Airflow para garantir migração e criação de usuario airflow 2

--- airflow/entrypoint.sh
+++ airflow/entrypoint.sh
@@ -10,3 +10,7 @@ airflow users create \
     --password primeup
 
 exec airflow standalone
+
+# a ideia desse entrypoint é garantir que o banco de dados esteja migrado e que o usuário admin seja criado antes de iniciar o Airflow.
+#Jenkins não passa a informação de usuário e senha do Airflow, então o entrypoint.sh é usado para criar um usuário admin padrão.
+# Pode ser alterado se necessário.
